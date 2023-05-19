9087451ab51a153c0cfe8df7d579fee1ed84c173
Loi <133347429+DuongVanLoi@users.noreply.github.com>
2023-05-19 23:13:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def get_detection_folder():
 
 if __name__ == '__main__':
 
-    st.title('YOLOv5 Streamlit App')
+    st.title('PHÁT HIỆN YOLO V5')
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', nargs='+', type=str,
@@ -70,16 +70,16 @@ if __name__ == '__main__':
     opt = parser.parse_args()
     print(opt)
 
-    source = ("图片检测", "视频检测")
-    source_index = st.sidebar.selectbox("选择输入", range(
+    source = ("Hình Ảnh", "Video")
+    source_index = st.sidebar.selectbox("Lựa Chọn", range(
         len(source)), format_func=lambda x: source[x])
 
     if source_index == 0:
         uploaded_file = st.sidebar.file_uploader(
-            "上传图片", type=['png', 'jpeg', 'jpg'])
+            "Tải Hình Ảnh Lên", type=['png', 'jpeg', 'jpg'])
         if uploaded_file is not None:
             is_valid = True
-            with st.spinner(text='资源加载中...'):
+            with st.spinner(text='Tải Lên...'):
                 st.sidebar.image(uploaded_file)
                 picture = Image.open(uploaded_file)
                 picture = picture.save(f'data/images/{uploaded_file.name}')
@@ -87,10 +87,10 @@ if __name__ == '__main__':
         else:
             is_valid = False
     else:
-        uploaded_file = st.sidebar.file_uploader("上传视频", type=['mp4'])
+        uploaded_file = st.sidebar.file_uploader("Tải Video Lên", type=['mp4'])
         if uploaded_file is not None:
             is_valid = True
-            with st.spinner(text='资源加载中...'):
+            with st.spinner(text='Tải Lên...'):
                 st.sidebar.video(uploaded_file)
                 with open(os.path.join("data", "videos", uploaded_file.name), "wb") as f:
                     f.write(uploaded_file.getbuffer())
@@ -100,7 +100,7 @@ if __name__ == '__main__':
 
     if is_valid:
         print('valid')
-        if st.button('开始检测'):
+        if st.button('Phát Hiện'):
 
             detect(opt)
 
